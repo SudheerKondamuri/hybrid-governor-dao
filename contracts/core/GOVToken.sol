746b28c2a8fa89b9236f9224e2f6f1ec530a6715
SudheerKondamuri <sudheersudheer7654321@gmail.com>
2026-01-19 21:20:21 +0530

    feat: implement GOVToken.sol

---
+++ contracts/core/GOVToken.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+
+contract GOVToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor() 
+        ERC20("CryptoVentures Token", "CVT") 
+        ERC20Permit("CryptoVentures Token") 
+    {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
